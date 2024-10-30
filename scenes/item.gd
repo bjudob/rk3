589012a91e2fc26b7736d9e6f540ca22468ca5e8
Koncz Botond <konczbotondvagyok@gmail.com>
2024-10-31 01:56:07 +0200

    cursor change on item hover, next episode: pick it up

--- scenes/item.gd
+++ scenes/item.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var id = "fire"
 @export var image = "res://objects/fire.png"
+@onready var default_cursor = load("res://ui/cursor_dudu.png")
+@onready var pickup_cursor = load("res://ui/cursor_mochi.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +13,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_2d_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(pickup_cursor)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(default_cursor)
