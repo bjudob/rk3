e87b96dbbf1772f696aba85ece634714c58f6576
Koncz Botond <konczbotondvagyok@gmail.com>
2024-11-04 00:15:55 +0200

    soul addition works

--- enemy_character.gd
+++ enemy_character.gd
@@ -1,22 +1,22 @@
 class_name EnemyCharacter
 extends CharacterBody2D
 
-@export
-var max_hp = 100
-@export
-var animator:AnimationPlayer
+@export var max_hp = 100
+@export var souls = 50
+@export var is_demon = true
+@export var animator:AnimationPlayer
 var current_hp = max_hp
+
+@onready var game_ui = get_tree().get_nodes_in_group("game_ui")[0]
 
 signal health_changed
 
 func _ready() -> void:
 	pass
 
-
 func _process(delta: float) -> void:
 	if current_hp < 0:
-		self.queue_free()
-		health_changed.emit()
+		die()
 		
 
 func take_damage(dmg: int):
@@ -24,3 +24,8 @@ func take_damage(dmg: int):
 	animator.play("hurt")
 	health_changed.emit()
 	# also play sound
+
+func die():
+	self.queue_free()
+	health_changed.emit()
+	game_ui.add_souls(souls, is_demon)
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -7,9 +7,15 @@ extends CanvasLayer
 
 @onready var tooltip = $Tooltip
 @onready var inventory = $Inventory
+@onready var demon_souls_display = $DemonSouls/HBoxContainer/MarginContainer2/RichTextLabel
+@onready var angel_souls_display = $AngelSouls/HBoxContainer/MarginContainer2/RichTextLabel
+
+var demon_souls = 0
+var angel_souls = 0
 
 func _ready() -> void:
-	pass
+	demon_souls_display.text = str(demon_souls)
+	angel_souls_display.text = str(angel_souls)
 
 func show_tooltip(text: String):
 	tooltip.show_tooltip(text)
@@ -19,3 +25,11 @@ func hide_tooltip():
 
 func add_item(item: Item):
 	inventory.add_item(item)
+	
+func add_souls(nr: int, is_demon: bool):
+	if is_demon:
+		demon_souls+=nr
+		demon_souls_display.text = str(demon_souls)
+	else:
+		angel_souls+=nr
+		angel_souls_display.text = str(angel_souls)
