18812866657c492176884932c0e74861ed6a4bba
Koncz Botond <konczbotondvagyok@gmail.com>
2024-11-29 02:52:45 +0200

    a karacsonyi nyuszi

--- scripts/pingu.gd
+++ scripts/pingu.gd
@@ -3,4 +3,5 @@ extends NPC
 @export var animation_player: AnimationPlayer
 
 func _on_item_correct(item):
-	animation_player.play("pingu_szanko")
+	if animation_player:
+		animation_player.play("pingu_szanko")
