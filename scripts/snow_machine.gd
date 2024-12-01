caa80b41641d2890ee706a6c3a58f0ea7273e0e4
Koncz Botond <konczbotondvagyok@gmail.com>
2024-12-01 03:40:56 +0200

    snowmachineworks

--- scripts/snow_machine.gd
+++ scripts/snow_machine.gd
@@ -1,5 +1,17 @@
 extends Itemable
 
+var unlocked = false
+
+func _ready() -> void:
+	super._ready()
+	$Timer.connect("timeout", _on_timer_ends)
+
 func _on_item_correct(item):
+	unlocked = true
+	$CPUParticles2D.visible = true
+	$AudioStreamPlayer2D.play()
+	$Timer.start()
+
+func _on_timer_ends():
 	$"../SnowWall".visible = false
 	$"../SnowWall/CollisionShape2D".disabled = true
