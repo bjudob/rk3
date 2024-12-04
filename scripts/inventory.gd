497701244f9e9c26bfa76f0bd2ee632d5ee023e6
Koncz Botond <konczbotondvagyok@gmail.com>
2024-12-05 01:07:49 +0200

    jezi fix

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -53,7 +53,7 @@ func refresh_ui():
 		if i < items.size():
 			item = items[i]
 		item_slot.set_item(item)
-		if i >= items.size() and i >= 5:
+		if i >= items.size() and i >= 10:
 			item_slot.visible = false
 		else:
 			item_slot.visible = true
--- scripts/jezi_heaven.gd
+++ scripts/jezi_heaven.gd
@@ -1,19 +1,20 @@
 extends NPC
 
 func _ready() -> void:
-	visible = false
+	super._ready()
 	$TalkArea/CollisionShape2D.disabled = true
 	$ItemableArea/CollisionShape2D.disabled = true
-	$ItemNail.visible = false
 	$ItemNail/Area2D/CollisionShape2D.disabled = true
+	visible = false
+	$ItemNail.visible = false
 
 func _process(delta: float) -> void:
 	super._process(delta)
 	if main.event_happened(Main.GameEvents.JEZI_RESPAWN):
 		visible = true
+		$ItemNail.visible = true
 		$TalkArea/CollisionShape2D.disabled = false
 		$ItemableArea/CollisionShape2D.disabled = false
-		$ItemNail.visible = true
 		$ItemNail/Area2D/CollisionShape2D.disabled = false
 	
 func _on_item_correct(item):
